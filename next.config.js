c1f99e4c580489eb5a8f1eedb7dc020d31a5e293
Minus960703 <cgzoo9607@gmail.com>
2023-01-03 15:06:51 +0900

    [Chore] Build Test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/filteringAccommodationList",
+        destination: 'https://sg.petandbe.com/filtering/accommodation',
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
