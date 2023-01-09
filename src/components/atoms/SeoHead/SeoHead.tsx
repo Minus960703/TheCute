55a24a4a1ac8722f2f668dc49cfcbe71c73117fa
Minus960703 <cgzoo9607@gmail.com>
2023-01-09 15:47:23 +0900

    [Chore] Seo Test

--- src/components/atoms/SeoHead/SeoHead.tsx
+++ src/components/atoms/SeoHead/SeoHead.tsx
@@ -10,6 +10,7 @@ const SeoHead = ({title}: HeadProps) => {
   return (
     <Head>
       <title>{title}</title>
+      <meta name="google-site-verification" content='X3tjBCoHdIZb89hscOuUD6-xep1sGtP053Y1t_GzqLE' />
       <meta name="description" content={'더 귀여워 홍대점 강아지 카페'} />
       <meta name="keywords" content={'dog, 강아지, 카페, 홍대, 더귀여워, 더 귀여워, 강아지카페, cat, 고양이, 고양이카페'} />
       <meta property="og:title" content={title} />
