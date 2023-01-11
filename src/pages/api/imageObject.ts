f0baaff715af1d6dbb5b9f15fed09cfcd4a5733b
Jinwoo Choi <cgzoo9607@gmail.com>
2023-01-12 00:42:01 +0900

    [Fix] DataObject Fixed

--- src/pages/api/imageObject.ts
+++ src/pages/api/imageObject.ts
@@ -4,7 +4,121 @@ const ImageDatas = [
     category: 'dog',
     name: 'buzzi',
     imgUrl: 'https://velog.velcdn.com/images/cjw960703/post/ad2ac466-addf-48dc-9e31-5fb5703a8942/image.jpeg'
-  }
+  },
+  {
+    id: 2,
+    category: 'dog',
+    name: 'buzzi',
+    imgUrl: 'https://velog.velcdn.com/images/cjw960703/post/763f1c38-5fea-4d4c-aced-748e0c01a5f7/image.jpeg'
+  },
+  {
+    id: 3,
+    category: 'dog',
+    name: 'buzzi',
+    imgUrl: 'https://velog.velcdn.com/images/cjw960703/post/b5e3ea96-856c-4918-a96f-5ad236a58999/image.jpeg'
+  },
+  {
+    id: 4,
+    category: 'dog',
+    name: 'buzzi',
+    imgUrl: 'https://velog.velcdn.com/images/cjw960703/post/77e6f543-de6c-416f-9cd8-5d2999da5bc8/image.jpeg'
+  },
+  {
+    id: 5,
+    category: 'dog',
+    name: 'buzzi',
+    imgUrl: 'https://velog.velcdn.com/images/cjw960703/post/0de54ee3-aa17-4b24-a04b-afb503d99409/image.jpeg'
+  },
+  {
+    id: 6,
+    category: 'dog',
+    name: 'buzzi',
+    imgUrl: 'https://velog.velcdn.com/images/cjw960703/post/e0dbbb99-bd2a-46a7-83ad-3ccac1955f1c/image.jpeg'
+  },
+  {
+    id: 7,
+    category: 'dog',
+    name: 'buzzi',
+    imgUrl: 'https://velog.velcdn.com/images/cjw960703/post/8ed8b07f-bb83-491c-a565-7606aac6dcb3/image.jpeg'
+  },
+  {
+    id: 8,
+    category: 'dog',
+    name: 'buzzi',
+    imgUrl: 'https://velog.velcdn.com/images/cjw960703/post/38f5d67b-8a7f-404c-a4d5-4f71e937743b/image.jpeg'
+  },
+  {
+    id: 9,
+    category: 'dog',
+    name: 'buzzi',
+    imgUrl: 'https://velog.velcdn.com/images/cjw960703/post/6289852a-9485-4263-8a1a-a37756a9a196/image.jpeg'
+  },
+  {
+    id: 10,
+    category: 'dog',
+    name: 'buzzi',
+    imgUrl: 'https://velog.velcdn.com/images/cjw960703/post/91c57588-4d56-4d0b-b75b-c9657cc3acf8/image.jpeg'
+  },
+  {
+    id: 11,
+    category: 'dog',
+    name: 'buzzi',
+    imgUrl: 'https://velog.velcdn.com/images/cjw960703/post/6192800f-1caa-4d82-8650-aaf9c60c8545/image.jpeg'
+  },
+  {
+    id: 12,
+    category: 'dog',
+    name: 'buzzi',
+    imgUrl: 'https://velog.velcdn.com/images/cjw960703/post/26c82ac5-58b1-413a-ab8f-d11509103b29/image.jpeg'
+  },
+  {
+    id: 13,
+    category: 'dog',
+    name: 'buzzi',
+    imgUrl: 'https://velog.velcdn.com/images/cjw960703/post/79c3cbb4-5abb-4bac-b39f-13eb89505a4f/image.jpeg'
+  },
+  {
+    id: 14,
+    category: 'dog',
+    name: 'buzzi',
+    imgUrl: 'https://velog.velcdn.com/images/cjw960703/post/eb0dca06-fdb1-471b-9341-251503c5f3dc/image.jpeg'
+  },
+  {
+    id: 15,
+    category: 'dog',
+    name: 'buzzi',
+    imgUrl: 'https://velog.velcdn.com/images/cjw960703/post/4cf8650e-2f9f-4924-8a1a-5e1c1fa226d4/image.jpeg'
+  },
+  {
+    id: 16,
+    category: 'dog',
+    name: 'buzzi',
+    imgUrl: 'https://velog.velcdn.com/images/cjw960703/post/fabe427d-ee3e-497f-942f-27e724227409/image.jpeg'
+  },
+  {
+    id: 17,
+    category: 'dog',
+    name: 'buzzi',
+    imgUrl: 'https://velog.velcdn.com/images/cjw960703/post/0a267051-c794-4d94-a443-e4ea751936b3/image.jpeg'
+  },
+  {
+    id: 18,
+    category: 'dog',
+    name: 'buzzi',
+    imgUrl: 'https://velog.velcdn.com/images/cjw960703/post/c8141bc1-d4fa-4587-812a-a29e9156ef8b/image.jpeg'
+  },
+  {
+    id: 19,
+    category: 'dog',
+    name: 'buzzi',
+    imgUrl: 'https://velog.velcdn.com/images/cjw960703/post/7dbc9f69-0b42-4b93-ad25-bf4ac8d85ea7/image.jpeg'
+  },
+  {
+    id: 20,
+    category: 'dog',
+    name: 'buzzi',
+    imgUrl: 'https://velog.velcdn.com/images/cjw960703/post/23a96d94-3ee1-4e1a-b750-aafd8f27bfb9/image.jpeg'
+  },
 ]
 
 export { ImageDatas } 
