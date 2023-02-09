f86c73200dac903ae24354852cb3605880fa22ba
Minus960703 <cgzoo9607@gmail.com>
2023-02-09 18:01:24 +0900

    [Feat] MenuSelectLinkOpen Function Making...

--- src/components/molecules/HomeMenu/HomeMenu.tsx
+++ src/components/molecules/HomeMenu/HomeMenu.tsx
@@ -4,23 +4,21 @@ import styles from './HomeMenu.module.scss';
 import { MenuProps } from '../../organisms/Header';
 
 const HomeMenu = ({ menu }: MenuProps) => {
-  const [menuList, setMenuList]= useState([...MenuList]);
-  const isActiveMenuItem = ({ id = 1 }: MenuListProps) => {
-    console.log(menuList)
-    setMenuList((prev) => ({
-      ...prev,
-      active: true
-    }))
-    console.log(menuList)
-
-    // setMenuList((prev: MenuListProps) => ({
-      
-    // }))
-  }
+  const [menuList, setMenuList] = useState([...MenuList]);
+  console.log(menuList)
+  // const isActiveMenuItem = (id: number) => {
+  //   menuList.map((prev) => {
+  //     prev.id === id
+  //       && setMenuList((prevData) => ({
+  //         ...prevData,
+  //         active: !prev.active
+  //       }));
+  //   });
+  // }
   return (
     <>
       { !menu 
-          &&  <ul className={styles.menu} onClick={isActiveMenuItem}>
+          &&  <ul className={styles.menu} onClick={ (e)=>console.log(e.target)}>
                 { menuList
                     && menuList.map((current)=><li key={current.id}>{current.content}</li>)
                 }
