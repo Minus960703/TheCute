2c018e286f869d91cccfde62bc5462a9e8159598
Minus960703 <cgzoo9607@gmail.com>
2023-02-09 17:31:38 +0900

    [Fix] MenuOpen 진행중

--- src/components/molecules/HomeMenu/HomeMenu.tsx
+++ src/components/molecules/HomeMenu/HomeMenu.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import { Label } from '../../atoms/Label';
-import { MenuList } from './HomeMenuObject';
+import { useState} from 'react';
+import { MenuList, MenuListProps } from './HomeMenuObject';
 import styles from './HomeMenu.module.scss';
 import { MenuProps } from '../../organisms/Header';
 
 const HomeMenu = ({ menu }: MenuProps) => {
-  const menuList = [...MenuList];
+  const [menuList, setMenuList]= useState([...MenuList]);
+  const isActiveMenuItem = ({ id = 1 }: MenuListProps) => {
+    console.log(menuList)
+    setMenuList((prev) => ({
+      ...prev,
+      active: true
+    }))
+    console.log(menuList)
+
+    // setMenuList((prev: MenuListProps) => ({
+      
+    // }))
+  }
   return (
     <>
       { !menu 
-          &&  <div className={styles.menu}>
+          &&  <ul className={styles.menu} onClick={isActiveMenuItem}>
                 { menuList
-                    && menuList.map((current)=><Label content={current.content} fontSize={"16px"} fontWeight={"500"}></Label>)
+                    && menuList.map((current)=><li key={current.id}>{current.content}</li>)
                 }
-              </div>
+              </ul>
       }
     </>
   )
