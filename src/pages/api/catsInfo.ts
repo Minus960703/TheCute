63c7e2f2997c3423cbd6179e715ee980e1eaec47
Minus960703 <cgzoo9607@gmail.com>
2023-02-15 18:00:48 +0900

    [Feat] Cats Page Create

---
+++ src/pages/api/catsInfo.ts
@@ -0,0 +1,51 @@
+const Cats = {
+  title: '고양이',
+  animals: {
+    parentsDogs: [
+      {
+        name: '우리',
+        file: 'woori',
+        age: 6,
+        birth: '2021-10-22',
+        gender: 'M',
+        point: ['t1']
+      },
+      {
+        name: '무만이',
+        file: 'muman',
+        age: 6,
+        birth: '2021-10-22',
+        gender: 'W',
+        point: ['t2']
+      },
+    ],
+    childDogs: [
+      {
+        name: '벨리',
+        file: 'velly',
+        age: 3,
+        birth: '2021-10-22',
+        gender: 'W',
+        point: ['t3']
+      },
+      {
+        name: '치즈',
+        file: 'cheese',
+        age: 3,
+        birth: '2021-10-22',
+        gender: 'W',
+        point: ['t4']
+      },
+      {
+        name: '달이',
+        file: 'dal',
+        age: 3,
+        birth: '2021-10-22',
+        gender: 'W',
+        point: ['t5']
+      }
+    ]
+  }
+}
+
+export { Cats }
--- src/pages/cats/index.tsx
+++ src/pages/cats/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { SeoHead } from '../../components/atoms/SeoHead';
+import { PomeranianInfo } from '../../components/molecules/PomeranianInfo';
+import { Cats } from '../api/catsInfo';
 
 const catsPage = () => {
   return (
     <section className='pages'>
       <SeoHead title='THE 귀여워 | 고양이' />
+      <PomeranianInfo animal={Cats}/>
     </section>
   )
 }
